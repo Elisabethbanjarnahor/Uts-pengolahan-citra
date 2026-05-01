dcfdc0d598157e6e6d54ec8854671bdb5758ca22
Elisabethbanjarnahor <elisabethmarbun34@gmail.com>
2026-05-01 13:44:54 +0700

    Create helper.py

---
+++ modules/helper.py
@@ -0,0 +1,11 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def buat_citra_sintetis(ukuran=256):
+    # ... [Paste isi fungsi buat_citra_sintetis di sini] ...
+    pass
+
+def tampilkan_hasil(judul_besar, gambar_list, judul_list, cmap_list=None, simpan=None):
+    # ... [Paste isi fungsi tampilkan_hasil di sini] ...
+    pass
